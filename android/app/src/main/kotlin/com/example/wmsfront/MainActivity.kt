9ec62f199751c89bc9a572991eeffb965dc13f6f
salomo07 <kenjibanzai@gmail.com>
2023-05-24 06:06:51 +0700

    Init

---
+++ android/app/src/main/kotlin/com/example/wmsfront/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wmsfront
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/size_config.dart';
+
+const wPrimaryColor = Color(0xFFFF7643);
+const wPrimaryLightColor = Color(0xFFFFECDF);
+const wPrimaryGradientColor = LinearGradient(
+  begin: Alignment.topLeft,
+  end: Alignment.bottomRight,
+  colors: [Color(0xFFFFA53E), Color(0xFFFF7643)],
+);
+const wSecondaryColor = Color(0xFF979797);
+const wTextColor = Color(0xFF757575);
+
+const wAnimationDuration = Duration(milliseconds: 200);
+
+final headingStyle = TextStyle(
+  fontSize: getProportionateScreenWidth(28),
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+  height: 1.5,
+);
+
+const defaultDuration = Duration(milliseconds: 250);
+
+// Form Error
+final RegExp emailValidatorRegExp =
+    RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+const String wEmailNullError = "Please Enter your email";
+const String wInvalidEmailError = "Please Enter Valid Email";
+const String wPassNullError = "Please Enter your password";
+const String wShortPassError = "Password is too short";
+const String wMatchPassError = "Passwords don't match";
+const String wNamelNullError = "Please Enter your name";
+const String wPhoneNumberNullError = "Please Enter your phone number";
+const String wAddressNullError = "Please Enter your address";
+
+final otpInputDecoration = InputDecoration(
+  contentPadding:
+      EdgeInsets.symmetric(vertical: getProportionateScreenWidth(15)),
+  border: outlineInputBorder(),
+  focusedBorder: outlineInputBorder(),
+  enabledBorder: outlineInputBorder(),
+);
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(getProportionateScreenWidth(15)),
+    borderSide: BorderSide(color: wTextColor),
+  );
+}
